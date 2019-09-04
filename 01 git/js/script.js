b26fe615db8db447be7c83f049e24524f76678a6
natalysiyah <natasha.artyuh@gmail.com>
2019-09-04 16:02:41 +0300

    Add java button events + - c

--- 01 git/js/script.js
+++ 01 git/js/script.js
@@ -1,14 +1,20 @@
-class Dog {
-    constructor(name, age){
-    this.name = name;
-    this.age = age;
-    }
+// class Dog {
+//     constructor(name, age){
+//     this.name = name;
+//     this.age = age;
+//     }
     
-    ShowDogInfo = () => {
-    console.log("Name => ", this.name, " Age => ", this.age);
-    }
-    }
+//     ShowDogInfo = () => {
+//     console.log("Name => ", this.name, " Age => ", this.age);
+//     }
+//     }
     
     
-    let Bobik = new Dog("Bobik", 3);
-    Bobik.ShowDogInfo();
+//     let Bobik = new Dog("Bobik", 3);
+//     Bobik.ShowDogInfo();
+// var screen=document.getElementsByClassName("screen");
+// var button=document.getElementsByClassName("button");
+// screen.innerHTML="0";
+// button.onclick=function click(){
+//     alert("ghjk");
+// }
